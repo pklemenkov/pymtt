fb67f7798d8237de65b8609e509af135973c136f
Pavel Klemenkov <pklemenkov@gmail.com>
2012-11-26 16:15:48 +0400

    Fix

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from distutils.core import setup
 
 setup(name='PyMTT',
-      version=1.0,
+      version='1.0',
       description='Python Config Generator',
       author='Pavel Klemenkov',
       author_email='pklemenkov@gmail.com',
       url='https://github.com/pklemenkov/pymtt',
       requires=[
-          'Jinja2>=2.3'
+          'Jinja2 (>=2.3)'
       ],
       scripts=[
           'bin/pymtt'
