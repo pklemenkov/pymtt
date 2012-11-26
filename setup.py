103c35a65a1fd643f81000bf6b526ecd6d4ded03
Pavel Klemenkov <pklemenkov@gmail.com>
2012-11-26 16:26:16 +0400

    Changed distutils to setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
 
-setup(name='PyMTT',
+setup(name='pymtt',
       version='1.0',
       description='Python Config Generator',
       author='Pavel Klemenkov',
       author_email='pklemenkov@gmail.com',
       url='https://github.com/pklemenkov/pymtt',
       requires=[
-          'Jinja2 (>=2.3)'
+          'Jinja2 >=2.3'
       ],
       scripts=[
           'bin/pymtt'
